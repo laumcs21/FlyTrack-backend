14c4753a01df8ff0dfa664437822b6aab658668a
laumcs21 <lauram.cardenass@uqvirtual.edu.co>
2026-04-08 08:00:00 -0500

    Proyecto inicial

---
+++ src/main/java/com/flytrack/FlytrackApplication.java
@@ -0,0 +1,13 @@
+package com.flytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlytrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flytrack/FlytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
